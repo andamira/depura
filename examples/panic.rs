bd8e3d49788a371f8108b733b933f4d71571da86
joseLuís <joseluis@andamira.net>
2023-02-09 13:26:42 +0100

    rustfmt

--- examples/panic.rs
+++ examples/panic.rs
@@ -1,10 +1,8 @@
-
 // - https://doc.rust-lang.org/stable/std/panic/struct.PanicInfo.html
 
 use std::panic;
 
-fn main () {
-
+fn main() {
     // A)
     // panic::set_hook(Box::new(|info| {
     //     if let Some(s) = info.payload().downcast_ref::<&str>() {
@@ -16,7 +14,6 @@ fn main () {
     //
     // panic!("Normal panic");
 
-
     // B)
 
     // panic::set_hook(Box::new(|s| { println!("Custom panic hook {s:?}"); }));
@@ -25,8 +22,6 @@ fn main () {
     //
     // // make a panic
     // let mut a = vec![1][1];
-
-
 
     // let result = panic::catch_unwind(|| {
     //     println!("hello!");
@@ -40,8 +35,8 @@ fn main () {
     // dbg![&result];
     // assert!(result.is_err());
 
-    assert![panic::catch_unwind(|| println!("hello!") ).is_ok()];
-    assert![panic::catch_unwind(|| panic!("oh no!") ).is_err()];
+    assert![panic::catch_unwind(|| println!("hello!")).is_ok()];
+    assert![panic::catch_unwind(|| panic!("oh no!")).is_err()];
 
     println!("hey");
 }
