4f41438e46a355d7cb2e0420f989029556c8b3d8
joseLuís <joseluis@andamira.net>
2023-03-16 09:45:04 +0100

    fix no_std build

--- src/logger/mod.rs
+++ src/logger/mod.rs
@@ -6,9 +6,11 @@
 #[cfg(feature = "std")]
 #[cfg_attr(feature = "nightly", doc(cfg(feature = "std")))]
 mod single;
+#[cfg(feature = "std")]
 pub use single::Logger;
 
 #[cfg(feature = "std")]
 #[cfg_attr(feature = "nightly", doc(cfg(feature = "std")))]
 mod multi;
+#[cfg(feature = "std")]
 pub use multi::MultiLogger;
