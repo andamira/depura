bdcf1ffa3b3d0006cd63fb96ee1288290b8183ce
joseLuís <joseluis@andamira.net>
2023-08-24 11:34:22 +0200

    update `lib` module
    - rename reexported `log` crate to `log_crate` and uninline it.
    - remove unneeded warning allowances.
    - update docs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,9 @@
-// libera::lib
+// depura::lib
 //
 //!
 //
 
 #![warn(clippy::all)]
-#![allow(
-    clippy::float_arithmetic,
-    clippy::implicit_return,
-    clippy::needless_return,
-    clippy::blanket_clippy_restriction_lints,
-    clippy::pattern_type_mismatch
-)]
 //
 #![cfg_attr(not(feature = "std"), no_std)]
 #![cfg_attr(feature = "safe", forbid(unsafe_code))]
@@ -22,9 +15,8 @@ compile_error!("You can't enable the `std` and `no-std` features at the same tim
 #[cfg(all(feature = "safe", feature = "unsafe"))]
 compile_error!("You can't enable the `safe` and `unsafe` features at the same time.");
 
-/// The *re-exported* [`log`][::log] crate:
-#[doc(inline)]
-pub use ::log as _log;
+/// The *re-exported* [`log`][log_crate] crate:
+pub use ::log as log_crate;
 
 pub mod error;
 pub mod logger;
@@ -34,7 +26,7 @@ pub mod timer;
 #[doc(inline)]
 pub use logger::*;
 
-/// Everything is directly available here.
+/// All the items are reexported here.
 ///
 /// More precisely everything defined in this crate.
 /// Not everything from the re-exported crates.
@@ -42,5 +34,5 @@ pub mod all {
     #[doc(inline)]
     pub use super::{error::*, logger::*, timer::*};
 
-    pub use super::_log::{debug, error, info, log, trace, warn, Level, LevelFilter};
+    pub use super::log_crate::{debug, error, info, log, trace, warn, Level, LevelFilter};
 }
